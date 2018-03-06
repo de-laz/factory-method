b5b7a4a860971ee1a77329df19bd97ba7d3078d4
Lazola Sifuba <sifubalazola@gmail.com>
2018-03-06 22:45:18 +0200

    Add IFactory interface

---
+++ src/FactoryMethod/AssemblyNotFoundException.cs
@@ -0,0 +1,10 @@
+// Copyright (c) de-laz. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace FactoryMethod
+{
+    using System;
+
+    public class AssemblyNotFoundException : Exception
+    {}
+}

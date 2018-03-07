29512dec53a06558350b33e98d993af3394c9f7c
Lazola Sifuba <sifubalazola@gmail.com>
2018-03-07 17:24:06 +0200

    Implement Factory create method

--- src/FactoryMethod/Factory.cs
+++ src/FactoryMethod/Factory.cs
@@ -3,41 +3,42 @@
 
 namespace FactoryMethod
 {
-    using System;
-    using System.Linq;
-    using System.Reflection;
+	using System;
+	using System.Linq;
+	using System.Reflection;
 
-    internal class Factory : IFactory
-    {
-        private readonly Assembly _assemblyToScan;
-        private const string NO_CREATOR_ERROR_MESSAGE =
-            @"There is no creator found for creating an instance of {0} from an instance of {1}";
-        
-        internal Factory(Assembly assemblyToScan) =>
-            _assemblyToScan = assemblyToScan ?? throw new ArgumentNullException(nameof(assemblyToScan));
+	internal class Factory : IFactory
+	{
+		private readonly Assembly _assemblyToScan;
+		private const string NO_CREATOR_ERROR_MESSAGE =
+			@"There is no creator found for creating an instance of {0} from an instance of {1}";
 
-        public TDestination Create<TSource, TDestination>(TSource source)
-            where TSource : class
-            where TDestination : class
-        {
-            throw new InvalidOperationException();
-        }
+		internal Factory(Assembly assemblyToScan) =>
+			_assemblyToScan = assemblyToScan ?? throw new ArgumentNullException(nameof(assemblyToScan));
 
-        private ICreator<TSource, TDestination> GetCreator<TSource, TDestination>(TSource source)
-            where TSource : class
-            where TDestination : class
-        {
-            var creatorType = _assemblyToScan.GetTypes()
+		public TDestination Create<TSource, TDestination>(TSource source)
+			where TSource : class
+			where TDestination : class
+		{
+			var creator = GetCreator<TSource, TDestination>(source);
+			return creator.Create(source);
+		}
+
+		private ICreator<TSource, TDestination> GetCreator<TSource, TDestination>(TSource source)
+			where TSource : class
+			where TDestination : class
+		{
+			var creatorType = _assemblyToScan.GetTypes()
 				.SingleOrDefault(type =>
-                    typeof(ICreator<TSource, TDestination>).IsAssignableFrom(type));
-            
-            if (creatorType is null)
-            {
-                throw new CreatorNotFoundException(
-                    string.Format(NO_CREATOR_ERROR_MESSAGE, typeof(TDestination), typeof(TSource)));
-            }
+					typeof(ICreator<TSource, TDestination>).IsAssignableFrom(type));
 
-            return (ICreator<TSource, TDestination>) Activator.CreateInstance(creatorType);
-        }
-    }
+			if (creatorType is null)
+			{
+				throw new CreatorNotFoundException(
+					string.Format(NO_CREATOR_ERROR_MESSAGE, typeof(TDestination), typeof(TSource)));
+			}
+
+			return (ICreator<TSource, TDestination>)Activator.CreateInstance(creatorType);
+		}
+	}
 }
